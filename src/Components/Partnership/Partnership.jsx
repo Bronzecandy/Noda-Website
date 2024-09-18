9d77e3f52320b1efcab0ee129ef9a21151f24e51
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2024-09-18 16:55:14 +0700

    reponsive partnership Program

--- src/Components/Partnership/Partnership.jsx
+++ src/Components/Partnership/Partnership.jsx
@@ -14,9 +14,11 @@ export const Partnership = () => {
                     <p>Collaborate with KOLs and streamers to promote the game and organize livestream events, giveaways, etc.</p>
                     <p>Join the partnership program to receive valuable gifts and special offers!</p>
                 </div>
-                <div className='PS_button'>
-                    <p>Contact us!</p>
-                </div>
+                <a href="#">
+                    <div className='PS_button'>
+                        <p>Contact us!</p>
+                    </div>
+                </a>
             </div>
         </div>
     );
