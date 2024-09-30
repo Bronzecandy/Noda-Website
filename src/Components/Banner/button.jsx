680d1fcd71a13e8a132453868a919ee5908cc290
Tran Chi Cong <tranchicong.dev@gmail.com>
2024-09-30 23:33:13 +0700

    Fix code

--- src/Components/Banner/button.jsx
+++ src/Components/Banner/button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../Partnership/Partnership.css';
 export default function Button({content}) {
   return (
-    <div className='PS_button' >
+    <div className='PS_button ' >
       <p>{content}</p>
     </div>
   )
